16aad7890bf092c7f56a58062e9f529ff667115a
gyugod93 <gyu4016@naver.com>
2025-04-26 18:56:33 +0900

    feat: 기초 프로젝트 세팅 / UI 컴포넌트 구조 세팅 / 컴포넌트 렌더링 방식 결정(초안)

---
+++ src/app/components/AirBnbItem.tsx
@@ -0,0 +1,14 @@
+//SSR렌더링 + 무한스크롤롤
+const AirBnbItem = () => {
+  return (
+    <>
+      <div>사진</div>
+      <div>사진명</div>
+      <div>거리</div>
+      <div>기간</div>
+      <div>가격/박</div>
+    </>
+  );
+};
+
+export default AirBnbItem;
